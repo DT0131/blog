ea9fe66ad02b6571122eafaca312b74d00694e22
d.toyoda <d.toyoda0131@gmail.com>
2020-04-24 09:50:42 +0900

    ヘッダーをハンバーガーメニューに変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,27 +9,24 @@
 </head>
 <body>
 @section('header')
-    <header>
-        <ul id="header-menu">
-            <li style="text-align: left">
-                <h1 class="site-title-in-header">おもちの毎日</h1>
-            </li>
-            <li>
-                <a href="{{ route('top.index') }}" class="header-link">Home</a>
-            </li>
-{{--            <li>--}}
-{{--                <a href="https://irishpub398141333.wordpress.com/%e3%81%93%e3%81%ae%e3%82%b5%e3%82%a4%e3%83%88%e3%81%ab%e3%81%a4%e3%81%84%e3%81%a6/" class="header-link">このサイトについて</a>--}}
-{{--            </li>--}}
-{{--            <li>--}}
-{{--                <a href="https://irishpub398141333.wordpress.com/%e3%81%8a%e5%95%8f%e3%81%84%e5%90%88%e3%82%8f%e3%81%9b/" class="header-link">お問い合わせ</a>--}}
-{{--            </li>--}}
-            <li>
-                <a target="_blank" href="https://www.instagram.com/omochi_no_iru_seikatsu/?hl=ja" class="header-link btn-social-long-instagram">
-                    <i class="fab fa-instagram"></i> <span>Follow Me</span>
+    <div id="header-bar">
+        <h1 class="site-title-in-header">おもちの毎日</h1>
+        <div id="app">
+            <Slide right>
+                <a href="{{ route('top.index') }}">
+                    <span>Home</span>
                 </a>
-            </li>
-        </ul>
-    </header>
+                <a target="_blank" href="https://www.instagram.com/omochi_no_iru_seikatsu/?hl=ja">
+                    <span>Instagram</span>
+                </a>
+                <a id="contact" href="#">
+                    <span>Contact</span>
+                </a>
+            </Slide>
+            <div id="page-wrap"></div>
+        </div>
+        <script src="{{mix('js/app.js')}}"></script>
+    </div>
 @show
 
 @yield('content')
@@ -40,12 +37,6 @@
             <li>
                 <a href="{{ route('top.index') }}" class="header-link">Home</a>
             </li>
-{{--            <li>--}}
-{{--                <a href="https://irishpub398141333.wordpress.com/%e3%81%93%e3%81%ae%e3%82%b5%e3%82%a4%e3%83%88%e3%81%ab%e3%81%a4%e3%81%84%e3%81%a6/" class="header-link">このサイトについて</a>--}}
-{{--            </li>--}}
-{{--            <li>--}}
-{{--                <a href="https://irishpub398141333.wordpress.com/%e3%81%8a%e5%95%8f%e3%81%84%e5%90%88%e3%82%8f%e3%81%9b/" class="header-link">お問い合わせ</a>--}}
-{{--            </li>--}}
             <li>
                 <a target="_blank" href="https://www.instagram.com/omochi_no_iru_seikatsu/?hl=ja" class="header-link btn-social-long-instagram">
                     <i class="fab fa-instagram"></i> <span>Follow Me</span>
