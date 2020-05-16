01cb97b6d3267f09d2a8d446747a45657a6bc94c
d.toyoda <d.toyoda0131@gmail.com>
2020-05-16 11:11:22 +0900

    サイトマップの作成

---
+++ app/Http/Controllers/SiteMapController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Articles;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
+use Carbon\Carbon;
+
+class SiteMapController extends Controller
+{
+    public function sitemap()
+    {
+        $sitemap = App::make("sitemap");
+        $now = Carbon::now();
+        $sitemap->add(URL::to('/'), $now, '1.0', 'always');
+        $sitemap->add(URL::to('/top'), $now, '1.0', 'always');
+        $sitemap->add(URL::to('/contact'), $now, '0.8', 'always');
+
+        $articles = Articles::orderBy('updated_at', 'desc')->get();
+        foreach ($articles as $article) {
+            $sitemap->add(URL::to('/post/' . $article->id), $article->updated_at, '0.9', 'yearly');
+        }
+
+        return $sitemap->render('xml');
+    }
+}
